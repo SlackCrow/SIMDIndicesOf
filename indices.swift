068f7f31ba613f2f3dccd31425975393797e29a3
SlackCrow <jasonkim@linux.com>
2021-02-03 21:20:31 +0900

    Update indices.swift

--- indices.swift
+++ indices.swift
@@ -33,4 +33,39 @@ extension String {
         
         return toReturn
     }
+        func simdIndicesOfIntel(substring: String) -> Array<Int>{
+        var toReturn = Array<Int>()
+        var inputToUse = self
+        // lousy hack that pads the input until it is divisible by 32(or 16 if 128bit).
+        while(Int(Float(inputToUse.count).truncatingRemainder(dividingBy: 32.0)) != 0){
+            inputToUse = inputToUse + " "
+        }
+        var inputInInteger = Array(inputToUse.utf8)
+        let substringInInteger = Array(substring.utf8)
+        for _ in 1..<substring.count{
+            inputInInteger.append(32)
+        }
+        let firstLetterOfSubstring = unsafeBitCast(SIMD32<UInt8>(repeating: UInt8 (substringInInteger[0])), to: __m256i.self)
+        let secondLetterOfSubstring = unsafeBitCast(SIMD32<UInt8>(repeating: UInt8(substringInInteger[substring.count-1])), to: __m256i.self)
+        for i in 1..<((inputToUse.count - 1 + 32) / 32)+1 {
+            let firstPartToCheck = unsafeBitCast(SIMD32<UInt8>(inputInInteger[32 * (i-1)...(32 * i) - 1]), to: __m256i.self)
+            let secondPartToCheck = unsafeBitCast(SIMD32<UInt8>(inputInInteger[(32 * (i-1))+(substring.count-1)...((32 * i) - 1)+(substring.count-1)]), to: __m256i.self)
+            let FF = _mm256_cmpeq_epi8(firstPartToCheck, firstLetterOfSubstring)
+            let SE = _mm256_cmpeq_epi8(secondPartToCheck, secondLetterOfSubstring)
+            let andResult = _mm256_and_si256(FF, SE)
+            let maskResult = _mm256_movemask_epi8(andResult)
+            if(maskResult != 0)
+            {
+                let andResultConverted = unsafeBitCast(andResult, to: SIMD32<Int8>.self)
+                for j in 0..<32{
+                    if(andResultConverted[j] == -1){
+                        toReturn.append((i-1)*32+j)
+                    }
+                }
+            }
+        }
+        
+        return toReturn
+    }
+
 }
